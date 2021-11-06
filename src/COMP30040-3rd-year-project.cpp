c6fa6f2d9b265a3475252101cc5e0568a7ddfb34
Kostiantyn Perehuda <kostiantyn.perehuda@student.manchester.ac.uk>
2021-11-06 15:32:43 +0000

    Initial commit

---
+++ src/COMP30040-3rd-year-project.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : COMP30040-3rd-year-project.cpp
+// Author      : Kostiantyn Perehuda
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
