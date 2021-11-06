97c95c5f091bfc3f45cb60c23da8f3d75796fa6e
Kostiantyn Perehuda <kostiantyn.perehuda@student.manchester.ac.uk>
2021-11-06 23:09:31 +0000

    Exclude unnecessary object files from source folder

--- cvdrone/ardrone/ardrone.h
+++ cvdrone/ardrone/ardrone.h
@@ -61,8 +61,8 @@
 #include <opencv2/highgui/highgui.hpp>
 
 #include <opencv2/core/core_c.h>
-#include <opencv2/videoio/legacy/constants_c.h>
 #include <opencv2/highgui/highgui_c.h>
+//#include <opencv2/videoio/legacy/constants_c.h>
 
 // FFmpeg
 extern "C" {
